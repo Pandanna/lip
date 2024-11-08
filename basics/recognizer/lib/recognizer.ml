d8effba69703698a6dcc92a1d05df8c57f9caa26
Docker <docker@example.com>
2024-11-08 22:37:45 +0000

    Riconoscitori sistemati

--- basics/recognizer/lib/recognizer.ml
+++ basics/recognizer/lib/recognizer.ml
@@ -1,40 +1,29 @@
 let rec lang1 l = match l with 
   | [x] when x == '1' || x=='0' -> true
   | x :: l when x == '1' || x == '0' -> true && lang1 l 
-  |_ -> false ;;
+  |_ -> false 
 
 let rec lang2_rec l =  match l with 
   |[] -> true 
   | [x] when x == '0' || x == '1'  -> true 
   | '1' :: l -> true && lang2_rec l 
-  |_ -> false;;
+  |_ -> false
 
 let lang2 l = lang2_rec (List.rev(l));;
 
 let rec lang3_rec l i = match l with
-|['0'] -> true
-| '0' :: l when i = 0 -> true && lang3_rec l (i+1)
-| x :: l when x == '0' || x == '1' -> true && lang3_rec l (i+1)
-|_ -> false;;
-
-
-let rec lang3_rec l i = match l with
-  |['0'] when i >= 1 -> true
-  | '0' :: l when i = 0 -> true && lang3_rec l (i+1)
+  | '1' :: l when i = 0 -> false && lang3_rec l (i+1)
+  |['0'] when i >= 1 -> true 
   | x :: l when x == '0' || x == '1' -> true && lang3_rec l (i+1)
-  |_ -> false;;
+  |_ -> false
 
-let lang3 l = lang3_rec l 0 ;;
+let lang3 l = lang3_rec l 0 
 
 
-let rec lang4_rec l i = match l with
-  |[] -> true
-  | '0' :: l when i == 0 -> true && lang4_rec l i
-  | '1' :: '0' :: l -> true && lang4_rec l (i+1)
-  |_ -> false ;;
-
-let lang4 l = lang4_rec l 0 ;;
-
+let lang4 l = 
+  let count_ones = List.length (List.filter (fun a -> a = '1') l) in
+  let count_zeros = List.length (List.filter (fun a -> a = '0') l) in
+  count_ones = 2 && count_zeros = (List.length(l) - 2);;
 
 let rec lang5_rec l i = match l with 
   |[] when i > 0 -> true 
